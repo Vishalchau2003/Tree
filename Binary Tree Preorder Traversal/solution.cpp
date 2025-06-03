9369019f97080f2be81fa67159159ebbd7fcd725
Vishalchau2003 <cse23146@glbitm.ac.in>
2025-06-04 01:11:52 +0530

    Update solution.cpp

--- Binary Tree Preorder Traversal/solution.cpp
+++ Binary Tree Preorder Traversal/solution.cpp
@@ -27,3 +27,32 @@ public:
         return nums;
     }
 };
+
+
+//methode 2 
+//itrative approach
+
+
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> result;
+        if (root == nullptr)
+            return result;
+
+        stack<TreeNode*> st;
+        st.push(root);
+        
+        while (!st.empty()) {
+            TreeNode* node = st.top();
+            st.pop();
+            result.push_back(node->val);  
+            if (node->right != nullptr)
+                st.push(node->right);
+            if (node->left != nullptr)
+                st.push(node->left);
+        }
+
+        return result;
+    }
+};
