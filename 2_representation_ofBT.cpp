cd18e3c2c917fd6b646107afb8729e979f4cf49e
Vishalchau2003 <cse23146@glbitm.ac.in>
2026-01-23 22:10:15 +0530

    Update 2_representation_ofBT.cpp

--- 2_representation_ofBT.cpp
+++ 2_representation_ofBT.cpp
@@ -18,3 +18,40 @@ int main(){
    root->right=new Node(3);
 
 }
+
+
+
+//vs code
+
+#include<bits/stdc++.h>
+using namespace std;
+
+struct TreeNode{
+
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x): val(x),left(nullptr),right(nullptr){};
+};
+TreeNode* build(int i,int n,vector<int>&arr){
+    if(i>=n||arr[i]==-1) return;
+    TreeNode* root=new TreeNode(arr[i]);
+
+    root->left=build(2*i+1,n,arr);
+    root->right=build(2*i+2,n,arr);
+
+    return root;
+}
+int main(){
+    int n;
+    cout<<"enter the size of array";
+    cin>>n;
+    vector<int>arr(n);
+    cout<<"enter nodes of tree";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+
+    TreeNode* root=build(0,n,arr);
+    return 0;
+}
