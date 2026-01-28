91f3200cb8b1cc1f5e13f2b8d19570f429e456cc
Vishalchau2003 <cse23146@glbitm.ac.in>
2026-01-28 19:21:41 +0530

    Add files via upload

---
+++ 11_Check if two trees are identical or not.cpp
@@ -0,0 +1,60 @@
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+TreeNode* build(int n, vector<int>& arr) {
+    if (n == 0 || arr[0] == -1) return nullptr;
+
+    queue<TreeNode*> q;
+    TreeNode* root = new TreeNode(arr[0]);
+    q.push(root);
+
+    int i = 1;
+    while (!q.empty() && i < n) {
+        TreeNode* curr = q.front();
+        q.pop();
+
+        if (i < n && arr[i] != -1) {
+            curr->left = new TreeNode(arr[i]);
+            q.push(curr->left);
+        }
+        i++;
+
+        if (i < n && arr[i] != -1) {
+            curr->right = new TreeNode(arr[i]);
+            q.push(curr->right);
+        }
+        i++;
+    }
+    return root;
+}
+
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+      if(p==nullptr||q==nullptr){
+        return p==q;
+      }
+      //preorder traversal
+      return (p->val==q->val)&&isSameTree(p->left,q->left)&&isSameTree(p->right,q->right);
+    }
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    TreeNode* root = build(n, arr);
+
+    return 0;
+}
+
